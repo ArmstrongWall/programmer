e0f1838e8f0f95c026456e6e9b6714d083eb1209
ArmstrongWall <160111223wzq@gmail.com>
2017-08-17 14:30:00 +0800

    add state pattern some comment

--- cplusplus/3_state/src/context.cpp
+++ cplusplus/3_state/src/context.cpp
@@ -2,13 +2,17 @@
 #include"context.h"
 #include"state.h"
 
-
+/*
+ * Context在整个状态转换的过程中起着一个中间人的作用
+ * 好比两个人进行商品交易，他们都来到一个共同的地方
+ * 谈生意，在Context里面完成状态的交接
+ * */
 
 Context::Context(){}
 
 Context::Context(State* state)
 {
-    this->_state = state;
+    this->_state = state;//指代私有变量为传入参数
 }
 
 Context::~Context()
