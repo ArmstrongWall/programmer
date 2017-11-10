79acc42fad7c1835276ae3f431dbd7b7f897de96
ArmstrongWall <160111223wzq@gmail.com>
2017-11-10 13:40:09 +0800

    finish reading D&A CH1,do homework

---
+++ read_book/c1_guide/m1_18_pointer_copy/src/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+class IntCell
+{
+public:
+                    //析构函数
+                    //拷贝构造函数
+                    //移动构造函数
+                    //拷贝赋值
+                    //移动赋值
+private:
+    int *stoedValue; // class contain pointer
+};
+
+
+int main(int argc,char** argv)
+{
+    
+    
+   
+    return 0;
+}
+
