8bf4ed1885f343a3e2b613cf4e62166132682957
wzq <ziqiang.wang@uisee.com>
2018-05-30 14:22:20 +0800

    cpp + 13

---
+++ cplusplus/13_reinterpret_cast/cmake-build-debug/CMakeFiles/13_reinterpret_cast.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/13_reinterpret_cast.dir/main.cpp.o"
+  "13_reinterpret_cast.pdb"
+  "13_reinterpret_cast"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/13_reinterpret_cast.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
