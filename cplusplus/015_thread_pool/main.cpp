fabdb9444d00cb41521e6f30c3a5476741012e99
CHEN-Chensheng <Chensheng.CHEN@outlook.com>
2018-10-06 22:23:05 +0800

    thread pull 2

--- cplusplus/015_thread_pool/main.cpp
+++ cplusplus/015_thread_pool/main.cpp
@@ -10,7 +10,7 @@ void workFunc1(void* data_) {
 int main() {
 
     double data1[13] = {1,2,3,4,5,6,7,8,9,10,11,12,13};
-    ThreadPool<double> threads(13);
+    ThreadPool<double> threads(13);//13 pool
     for(int i = 0; i < 13; ++i)
         threads.reduce(i, workFunc1, data1[i]);
     return 0;
