62de9d0f38e5ca34a50c9fd41cc92dcd5d939481
wzq <ziqiang.wang@uisee.com>
2019-05-14 10:21:03 +0800

    condition

--- read_book/concurrency_in_action/src/c4_1_condition_variable/condition_variable.cpp
+++ read_book/concurrency_in_action/src/c4_1_condition_variable/condition_variable.cpp
@@ -3,11 +3,11 @@
 //
 
 #include "condition_variable.h"
-#define NUM 10000
+#define NUM 10
 std::mutex	mut;
 std::queue<int>	data_queue;		//	1
 std::condition_variable	data_cond;
-
+bool ready = false;
 void process(const int &data) {
     std::cout << "process data = " << data << std::endl;
 }
@@ -18,34 +18,39 @@ bool is_last_chunk(const int &data) {
 
 void data_preparation_thread()
 {
+
+    usleep(5000*1000);
     int count = 0;
-    while(true) {
+    //while(true) {
         int	const data = count;
         std::lock_guard<std::mutex>	lk(mut);
         data_queue.push(data);		//	2
         std::cout << "push data = " << data << std::endl;
+        ready = true;
         data_cond.notify_one();		//	3
         count++;
-        if(count > NUM)
-            break;
-        usleep(5*1000);
-    }
+//        if(count > NUM)
+//            break;
+
+    //}
 }
 
 
 void data_processing_thread() {
-    while(true) {
+    //while(true) {
         std::unique_lock<std::mutex> lk(mut);		//	4
         data_cond.wait(
-                lk,[]{return !data_queue.empty();});		//	5
+                lk,[]{return ready;});		//	5
         int	data=data_queue.front();
-        data_queue.pop();
+    std::cout << "pop data = " << data << std::endl;
+
+    data_queue.pop();
         lk.unlock();		//	6
         process(data);
-        if(is_last_chunk(data))
-            break;
+//        if(is_last_chunk(data))
+//            break;
         usleep(20*1000);
-    }
+    //}
 }
 
 
@@ -56,3 +61,5 @@ void condition_variable_demo(){
     t1.join();
     t2.join();
 }
+
+
